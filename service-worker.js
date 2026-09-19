7a2d0a27ba084a8c2441acd08b574a9b44124c88
Shamba2026 <sandywillyz16@gmail.com>
2026-09-20 01:08:09 +0300

    Refresh gated app shell cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ngombe-herdbook-shell-v13";
+const CACHE_NAME = "ngombe-herdbook-shell-v14";
 const ASSETS = [
   "./","./index.html","./manifest.json","./styles/app.css",
   "./src/main.js","./src/ui.js","./src/auth.js","./src/config.js",
