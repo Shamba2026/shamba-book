9b42211b4ae8beac1a567d17c6cb2096c40c3a56
Shamba2026 <sandywillyz16@gmail.com>
2026-09-19 16:38:43 +0300

    Refresh cached shell after authentication update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ngombe-herdbook-shell-v4";
+const CACHE_NAME = "ngombe-herdbook-shell-v5";
 const ASSETS = [
   "./","./index.html","./manifest.json","./styles/app.css",
   "./src/main.js","./src/ui.js","./src/config.js",
