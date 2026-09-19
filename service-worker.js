22730cf23c4c0a6749351afc0976b81ea4eccfc9
Shamba2026 <sandywillyz16@gmail.com>
2026-09-19 17:05:47 +0300

    Refresh cached config while sync is paused

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ngombe-herdbook-shell-v7";
+const CACHE_NAME = "ngombe-herdbook-shell-v8";
 const ASSETS = [
   "./","./index.html","./manifest.json","./styles/app.css",
   "./src/main.js","./src/ui.js","./src/auth.js","./src/config.js",
