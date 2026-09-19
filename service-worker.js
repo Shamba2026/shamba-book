e6be3f52002228ff5aa1ba13a20a56725c551906
Shamba2026 <sandywillyz16@gmail.com>
2026-09-20 01:00:07 +0300

    Bump service worker cache generation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ngombe-herdbook-shell-v10";
+const CACHE_NAME = "ngombe-herdbook-shell-v11";
 const ASSETS = [
   "./","./index.html","./manifest.json","./styles/app.css",
   "./src/main.js","./src/ui.js","./src/auth.js","./src/config.js",
